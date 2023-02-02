b127141a0620c278008e91e7dc79dde14b63a2f6
bloges56 <brady.drew.logan@gmail.com>
2023-02-02 15:28:04 -0600

    encoding

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import seaborn as sns
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
 
 class AgeImputer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -15,7 +17,43 @@ class AgeImputer(BaseEstimator, TransformerMixin):
         X['Age'] = imputer.fit_transform(X[['Age']])
         return X
 
+class FeatureEncoder(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        encoder = OneHotEncoder()
+        matrix = encoder.fit_transform(X[['Embarked']]).toarray()
+
+        column_names = ["C", "S", "Q", "N"]
+
+        for i in range(len(matrix.T)):
+            X[column_names[i]] = matrix.T[i]
+
+        matrix = encoder.fit_transform(X[[+'Sex']]).toarray()
+
+        column_names = ["Female", "Male"]
+
+        for i in range(len(matrix.T)):
+            X[column_names[i]] = matrix.T[i]
+
+        return X
+
+class FeatureDropper(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.drop(["Embarked", "Name", "Ticket", "Cabin", "Sex", "N"], axis=1, errors="ignore")
+
 titanic_data = pd.read_csv('data/train.csv')
+
+pipeline = Pipeline(["ageimputer", AgeImputer(),
+                      "featureencoder", FeatureEncoder(),
+                      "featuredropper", FeatureDropper()             
+                    ])
+
+
 
 sns.heatmap(titanic_data.corr(), cmap='YlGnBu')
 
@@ -25,12 +63,16 @@ for train_indices, test_indices in split.split(titanic_data, titanic_data[["Surv
     strat_train_set = titanic_data.loc[train_indices]
     strat_test_set = titanic_data.loc[test_indices]
 
-plt.subplot(1,2,1)
-strat_train_set['Survived'].hist()
-strat_train_set['Pclass'].hist()
+strat_train_set = pipeline.fit_transform(strat_train_set)
 
-plt.subplot(1,2,2)
-strat_test_set['Survived'].hist()
-strat_test_set['Pclass'].hist()
+print(strat_train_set)
 
-plt.show()
+# plt.subplot(1,2,1)
+# strat_train_set['Survived'].hist()
+# strat_train_set['Pclass'].hist()
+
+# plt.subplot(1,2,2)
+# strat_test_set['Survived'].hist()
+# strat_test_set['Pclass'].hist()
+
+# plt.show()
